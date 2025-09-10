395000000e66d20f3dcc767d01da88b07dae1a1e
Davisqxf12 <147102987+Davisqxf12@users.noreply.github.com>
2025-09-10 17:43:47 -0300

    Add files via upload

---
+++ ProductClientHub.Exceptions/ExceptionsBase/ProductClientHubException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace ProductClientHub.Exceptions.ExceptionsBase
+{
+    public abstract class ProductClientHubException : SystemException
+    {
+        public ProductClientHubException(String errorMessage) : base(errorMessage)
+        {
+        }
+
+        public abstract List<string> GetErrors();
+        public abstract HttpStatusCode GetHttpStatusCode();
+    }
+}
---
+++ UseCases/Products/SharedValidator/RequestProductValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using ProductClientHub.Communication.Requests;
+
+namespace ProductClientHub.API.UseCases.Products.SharedValidator
+{
+    public class RequestProductValidator : AbstractValidator<RequestProductJson>
+    {
+        public RequestProductValidator()
+        {
+            RuleFor(product => product.Name).NotEmpty().WithMessage("Nome do Produto inválido.");
+            RuleFor(product => product.Brand).NotEmpty().WithMessage("Marca do Produto inválida.");
+            RuleFor(product => product.Price).GreaterThan(0).WithMessage("Preço para o Produto inválido.");
+        }
+    }
+}
